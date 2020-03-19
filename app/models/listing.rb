7104910f2052305912686b027804be9d5992aec5
nathanveach <nathanbveach@gmail.com>
2020-03-19 12:12:53 -0700

    Added validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
   has_one_attached :image
-  validates :image, presence: true
+  validates :image, :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
