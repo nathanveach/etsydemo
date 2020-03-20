6a1bf17c8bbd53b8387b70f7d645619077504864
nathanveach <nathanbveach@gmail.com>
2020-03-20 13:03:10 -0700

    Associated listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ActiveRecord::Base
   has_one_attached :image
   validates :image, :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_185119) do
+ActiveRecord::Schema.define(version: 2020_03_20_194229) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_185119) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
